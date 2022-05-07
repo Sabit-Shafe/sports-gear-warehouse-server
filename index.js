6643fc3e41e739750e69478c59fdbceb86954351
Sabitshafeexit(0clear <sabitshaafee@gmail.com>
2022-05-07 23:27:03 +0600

    jwt done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+var jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -9,13 +11,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      return res.status(401).send({ message: 'unauthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+          return res.status(403).send({ message: 'Forbidden access, Please dont try' });
+      }
+      console.log('decoded', decoded);
+      req.decoded = decoded;
+      next();
+  })
+}
+
 //user :dbuser1
 //passs :cX1tMoPpKq0czA7i
 
 //mongodb 
 
 
-const uri = "mongodb+srv://dbuser1:cX1tMoPpKq0czA7i@cluster0.om8d3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const uri = "mongodb+srv://dbuser1:cX1tMoPpKq0czA7i@cluster0.om8d3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.om8d3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -31,6 +50,14 @@ async function run() {
       res.send(Items);
     });
 
+     // AUTH
+     app.post('/login', async (req, res) => {
+      const user = req.body;
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
+          expiresIn: '1d'
+      });
+      res.send({ accessToken });
+  })
     app.get('/items/:id', async (req, res) => {
       const id = req.params.id;
       console.log(id);
@@ -97,7 +124,7 @@ async function run() {
         res.send(Items);
       }
       else {
-        res.status(403).send({ message: 'forbidden access' })
+        res.status(403).send({ message: 'forbidden access, Try again' })
       }
     })
 
