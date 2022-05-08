23e9d710dec5902901057262396bf6d9963942f8
Sabitshafeexit(0clear <sabitshaafee@gmail.com>
2022-05-08 14:36:12 +0600

    extra section api done

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ app.use(express.json());
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-      return res.status(401).send({ message: 'unauthorized access' });
+    return res.status(401).send({ message: 'unauthorized access' });
   }
   const token = authHeader.split(' ')[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-          return res.status(403).send({ message: 'Forbidden access, Please dont try' });
-      }
-      console.log('decoded', decoded);
-      req.decoded = decoded;
-      next();
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden access, Please dont try' });
+    }
+    console.log('decoded', decoded);
+    req.decoded = decoded;
+    next();
   })
 }
 
@@ -58,14 +58,14 @@ async function run() {
       res.send(Items);
     });
 
-     // AUTH
-     app.post('/login', async (req, res) => {
+    // AUTH
+    app.post('/login', async (req, res) => {
       const user = req.body;
-      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
-          expiresIn: '1d'
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1d'
       });
       res.send({ accessToken });
-  })
+    })
     app.get('/items/:id', async (req, res) => {
       const id = req.params.id;
       console.log(id);
@@ -80,25 +80,7 @@ async function run() {
       res.send(result);
     });
 
-    // app.put('/quantityUpdate',async(req,res)=>{
-    //   const id = req?.body?.id;
-    //   const dec= req?.body?.dec;
-    //   const filter = {_id:ObjectId(id)}
-    //      // this option instructs the method to create a document if no documents match the filter
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //       $set: {
-    //         _const: updateDoc = { $inc: { quantity: -1 } },
-    //         get const() {
-    //           return this._const;
-    //         },
-    //         set const(value) {
-    //           this._const = value;
-    //         },
-    //       },
-    //     };
-    //   const result = await products.updateOne(filter, updateDoc, options);
-    //   return res.send(result);
+
 
     app.put('/items/:id', async (req, res) => {
       const result = await ItemsCollection.updateOne(
