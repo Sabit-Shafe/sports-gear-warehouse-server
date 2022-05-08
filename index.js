df766a41c2a20ab982face0d6ef65a7b0cd50f3b
Sabitshafeexit(0clear <sabitshaafee@gmail.com>
2022-05-08 18:16:08 +0600

    adding heruku

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+// console.log()
 
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
